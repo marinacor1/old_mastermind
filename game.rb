7d8aed4c644dca3f52a310f1c288f2b4a9054d11
marinacor1 <marinaspam1234@gmail.com>
2016-02-01 19:06:00 -0700

    adding more to game method

--- game.rb
+++ game.rb
@@ -1,20 +1,20 @@
+require 'pry'
 class Game
-  attr_reader :guesses, :count, :output
+  attr_reader :guesses, :output, :correct
+  attr_accessor :count
 
   def initialize
-    colors = ["red", "green", "blue", "yellow"]
-    correct = ["red", "green", "blue", "yellow"]
+    colors = ["r", "g", "b", "y"]
+    @correct = ["r", "g", "b", "y"]
     # correct = []
-    # correct << colors[rand(1..3)]
-    # correct << colors[rand(1..3)]
-    # correct << colors[rand(1..3)]
-    # correct << colors[rand(1..3)]
+    # correct = [colors[rand(0..3)], colors[rand(0..3)], colors[rand(0..3)], colors[rand(0..3)]]
+    @count = 0
     beginning_explanation
   end
 
   def beginning_explanation
     puts "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game. What's your guess?"
-    output = gets
+    output = gets.chomp
       if output.length > 4
         "Your guess is too long. Try again."
       elsif output.length < 4
@@ -30,38 +30,57 @@ class Game
 
     def game_method(guesses)
       @count +=1
+      guesses = guesses.chars
       if guesses.eql?(correct)
         end_game
       else
-        index = 0
-        correct_num = 0
-        position_num = 0
-        if guesses[index] == correct[index]
-          position_num +=1
+        postion_number
+        correct_number
+        results
+      end
+    end
+
+
+    def position_number
+      index = 0
+      @position_num = 0
+      while index <= @correct.length
+        if guesses[index] == @correct.join[index]
+          @position_num +=1
+          index += 1
         else
           index +=1
         end
       end
-        puts "'#{guesses.upcase}' has #{correct_num} of the correct elements with #{postion_num} in the correct postions \n You've taken #{@count} guess"
+      @position_num
+    end
+
+    def correct_number
+      @correct_num = 0
+      @guesses.each do |color|
+        if correct.include?(color)
+          correct_num +=1
+        end
+      end
+      @correct_num
+    end
+
+    def results
+      puts "'#{guesses.upcase}' has #{correct_num} of the correct elements with #{postion_num} in the correct postions \n You've taken #{@count} guess"
     end
 
     def end_game
       puts "Congratulation! You guessed the sequence '#{correct.upcase}' in #{@count} guesses over #{minutes} minutes, #{seconds} seconds. \nDo you want to (p)lay again or (q)uit?"
       output = gets
-        if output == 'p' || output == 'play'
+        if output.chomp == 'p' || output.chomp == 'play'
           ng = Game.new
-        elsif output == 'q' || output == 'quit'
+        elsif output.chomp == 'q' || output.chomp == 'quit'
           exit_game
         else
           puts "This is not an option."
         end
 
     end
-
-
-
-
-
 
 
 end
--- mastermind.rb
+++ mastermind.rb
@@ -3,17 +3,15 @@ require_relative 'game'
 puts "Welcome to MASTERMIND"
 puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
 
-output = gets
-g = Game.new
+output = gets.chomp
   if output == 'p'|| output == 'play'
-    g.play
-    #why is this running if play doesn't exist
+    g = Game.new
+    g.beginning_explanation
   elsif output == 'i'|| output == 'instructions'
+    g = Game.new
     g.instructions
   elsif output == 'q' || output == 'quit'
     #exit game
   else
     puts "This is not a valid option. Try again."
   end
-
-g.beginning_explanation
